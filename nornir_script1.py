e371853e89fc157d64da23ec1d5860669b44be14
jossysisay30 <joosy.sisay@gmail.com>
2020-06-07 15:10:34 +0000

    nonari

---
+++ nornir_script1.py
@@ -0,0 +1,11 @@
+from nornir import InitNornir
+
+nr = InitNornir("config.yml")
+
+from nornir.plugins.tasks.networking import netmiko_send_command
+
+from nornir.plugins.functions.text import print_result
+
+result = nr.run(netmiko_send_command, command_string="sh ip int brief")
+
+print_result(result)
